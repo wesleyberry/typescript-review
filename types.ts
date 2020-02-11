a994c2a3493f25eebc0de021865df1614f194028
Wesley Berry <wesleyberry52@gmail.com>
2020-02-11 13:33:41 -0600

    reviewed types

---
+++ types.ts
@@ -0,0 +1,38 @@
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[];
+// or ----
+let strArr2: Array<string>;
+let numArr2: Array<number>;
+let boolArr2: Array<boolean>;
+
+let strNumTuple: [string, number];
+let myVoid: void = undefined;
+let myNull: null = null;
+let myUndefined: undefined = null;
+
+myString = 'Hello'.slice(0, 3);
+myNum = 5.5;
+myBool = false;
+myVar = 5;
+strArr = ['Hello', 'World'];
+numArr = [1, 2, 3];
+boolArr = [true, false, false, true];
+strNumTuple = ['Hello', 4];
+
+console.log(myString);
+console.log(myNum);
+console.log(myBool);
+console.log(myVar);
+console.log(strArr);
+console.log(numArr);
+console.log(boolArr);
+console.log(strNumTuple);
+console.log(myVoid);
+console.log(myNull);
+console.log(myUndefined);
